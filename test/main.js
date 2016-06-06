bcb1051472eb08be8d72a045ea1629bf0677da67
Leonard Schuetz <leni.schuetz@me.com>
2016-06-06 17:48:16 +0200

    Removed unused dependency

--- test/main.js
+++ test/main.js
@@ -1,10 +1,10 @@
 const {
     textToImageBuffer,
     imageBufferToText
-} = require('../lib/index.js');
-const streamToBuffer = require('stream-to-buffer');
+} = require('../lib/index.js'); // require('txt-png');
 
 const message = 'Hello World. This is a test!';
+
 textToImageBuffer(message, (buffer) => {
     imageBufferToText(buffer, (text) => {
         console.log(text.length, message.length);
