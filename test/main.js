1917eab2e3238dc968e5aede88d77b0463dde9f5
Leonard Schuetz <leni.schuetz@me.com>
2016-06-08 20:58:27 +0200

    Error handling

--- test/main.js
+++ test/main.js
@@ -7,7 +7,6 @@ const message = 'Hello World. This is a test!';
 
 textToImageBuffer(message, (buffer) => {
     imageBufferToText(buffer, (text) => {
-        console.log(text.length, message.length);
         if (text === message) {
             console.log('[ OK ] Test passed!');
         } else {
